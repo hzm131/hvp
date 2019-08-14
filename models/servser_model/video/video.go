b3b2d3486722ae1ddf895f2bcbe26c4238d292ca
wildcat-man <wmmxsw@163.com>
2019-08-14 12:48:05 +0800

    设置指针类型 就可以存储null

--- models/servser_model/video/video.go
+++ models/servser_model/video/video.go
@@ -7,19 +7,19 @@ import (
 
 type Video struct {
 	servser_model.Model
-	Name         string  `gorm:"column:name"json:"name"` //片名
-	Pid          int     `gorm:"column:pid"json:"pid"`
-	Origin       string  `gorm:"column:origin"json:"origin"`             //产地
-	Duration     string  `gorm:"column:duration"json:"duration"`         //时长
-	Language     string  `gorm:"column:language"json:"language"`         //语种
-	Years        string  `gorm:"column:years"json:"years"`               //年份
-	Score        float32 `gorm:"column:score"json:"score"`               //评分
-	Introduction string  `gorm:"column:introduction"json:"introduction"` //简介
-	Category     string  `gorm:"column:category"json:"category"`         //类别
+	Name         *string  `gorm:"column:name"json:"name"` //片名
+	Pid          *int     `gorm:"column:pid"json:"pid"`
+	Origin       *string  `gorm:"column:origin"json:"origin"`             //产地
+	Duration     *string  `gorm:"column:duration"json:"duration"`         //时长
+	Language     *string  `gorm:"column:language"json:"language"`         //语种
+	Years        *string  `gorm:"column:years"json:"years"`               //年份
+	Score        *float32 `gorm:"column:score"json:"score"`               //评分
+	Introduction *string  `gorm:"column:introduction"json:"introduction"` //简介
+	Category     *string  `gorm:"column:category"json:"category"`         //类别
 
-	VideoSrcId int      `gorm:"column:video_src_id;"json:"video_src_id"validate:"required||integer"` //视频路径
+	VideoSrcId int      `gorm:"column:video_src_id;not null"json:"video_src_id"validate:"required||integer"` //视频路径
 	VideoSrc   VideoSrc `gorm:"ForeignKey:VideoSrcI;AssociationForeignKey:ID"json:"video_src"`
-	ImageSrcId int      `gorm:"column:image_src_id;"json:"image_src_id"validate:"required||integer"` //封面路径
+	ImageSrcId int      `gorm:"column:image_src_id;not null"json:"image_src_id"validate:"required||integer"` //封面路径
 	ImageSrc   ImageSrc `gorm:"ForeignKey:ImageSrcId;AssociationForeignKey:ID"json:"image_src"`
 }
 
@@ -88,10 +88,19 @@ func (this *Video) QueryVideos(condition string, orderBy string, limit string, o
 	}
 	count := servser_model.Db.Raw("select * from video left join video_src on video.video_src_id = video_src.id left join image_src on video.image_src_id = image_src.id where concat(name,origin) like ?", &cond).Scan(&totalVideo.Videos).RowsAffected
 	totalVideo.Total = int(count)
-
-	query := servser_model.Db.Raw("select * from video left join video_src on video.video_src_id = video_src.id left join image_src on video.image_src_id = image_src.id where concat(name,origin) like ? order by ? Desc limit ? offset ?", &cond, &orderBy, &limit, &offset).Scan(&totalVideo.Videos)
-	if err = query.Error; err != nil {
+	totalVideo.Videos = nil
+	rows, err := servser_model.Db.Raw("select video.id,name,pid,origin,duration,language,years,score,introduction,category,video_src_id,image_src_id,image_src.id,image_src.src_path,video_src.id,video_src.src_path from video left join video_src on video.video_src_id = video_src.id left join image_src on video.image_src_id = image_src.id where concat(name,origin) like ? order by ? Desc limit ? offset ?", &cond, &orderBy, &limit, &offset).Rows()
+	if err != nil {
 		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		video := Video{}
+		err = rows.Scan(&video.ID, &video.Name, &video.Pid, &video.Origin, &video.Duration, &video.Language, &video.Years, &video.Score, &video.Introduction, &video.Category, &video.VideoSrcId, &video.ImageSrcId, &video.ImageSrc.ID, &video.ImageSrc.SrcPath, &video.VideoSrc.ID, &video.VideoSrc.SrcPath)
+		if err != nil {
+			fmt.Println("errrrrrrr", err)
+		}
+		totalVideo.Videos = append(totalVideo.Videos, video)
 	}
 	return
 }
